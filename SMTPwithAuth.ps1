d30c11142ad4c1565cb2f1962c08523d4a645dd6
maxsobolev97 <77441161+maxsobolev97@users.noreply.github.com>
2022-03-24 13:37:18 +0300

    Add files via upload

---
+++ SMTPwithAuth.ps1
@@ -0,0 +1,22 @@
+﻿$serverSmtp = "example" 
+$port = 25
+$From = "example@example.ru" 
+$To = "example@example.ru" 
+$subject = "Письмо с вложением"
+$user = "example"
+$pass = "example"
+$file = "D:\example.txt"
+$att = New-object Net.Mail.Attachment($file)
+$mes = New-Object System.Net.Mail.MailMessage
+$mes.From = $from
+$mes.To.Add($to) 
+$mes.Subject = $subject 
+$mes.IsBodyHTML = $true 
+$mes.Body = "<h1>Тестовое письмо</h1>"
+$mes.Attachments.Add($att) 
+$smtp = New-Object Net.Mail.SmtpClient($serverSmtp, $port)
+$smtp.EnableSSL = $false 
+$smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass);
+$smtp.Send($mes)
+$a = $smtp.SendCompleted 
+$att.Dispose()
